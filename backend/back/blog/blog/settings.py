fa286c22cb92f6f1b5b80afcf273feda1c69675d
akash1106 <akashavt001@gmail.com>
2023-09-03 18:13:21 +0530

    context done

--- backend/back/blog/blog/settings.py
+++ backend/back/blog/blog/settings.py
@@ -37,13 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'data',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
